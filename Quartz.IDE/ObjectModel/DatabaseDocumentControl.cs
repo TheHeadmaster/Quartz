927872dfa0cd8eb6528ab37aabe21036faa1784b
David Palmer <davidivpalmer@gmail.com>
2020-09-23 18:20:46 -0500

    :arrow_up: Update ReactiveProperty 7.4.1 :arrow_right: 7.4.1

--- Quartz.IDE/ObjectModel/DatabaseDocumentControl.cs
+++ Quartz.IDE/ObjectModel/DatabaseDocumentControl.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using PersistentEntity;
 
 namespace Quartz.IDE.ObjectModel
 {
     public abstract class DatabaseDocumentControl : DocumentControl, IDatabaseObject
     {
+        public int ID { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
     }
 }
